41554e8532dfd00c3a1b0b8198d0279baf1f3d01
Canon Tschikof <tschuky101@Fibonacci.local>
2019-03-14 23:18:16 -0600

    Added Edit Menu for Electron app. This was to allow copy and pasting.

--- inventory-labels/main.js
+++ inventory-labels/main.js
@@ -83,6 +83,18 @@ function createWindow(){
 			]
 		},
 		{
+			label: 'Edit',
+			submenu: [
+				{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+				{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+				{ type: "separator" },
+				{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+				{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+				{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+				{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+			]
+		},
+		{
 			role: 'window',
 			submenu: [
 				{role: 'minimize'},
